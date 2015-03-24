b92b09f754d6c4ad8e236e0a2948fd29bc2a3a98
ratemybeard <teamdgroup@gmail.com>
2015-03-24 13:21:27 +0000

    Update forms.py

--- RMB/forms.py
+++ RMB/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from RMB.models import UserProfile, Images
-
+#form for uploading images
 class UploadImagesForm(forms.ModelForm):
 
     class Meta:
